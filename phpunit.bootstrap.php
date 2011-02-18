3edcddfb2c34b13df8013bb4cfee86051a44fe8b
Joshua Morris <php.rack@gmail.com>
2011-02-18 10:58:58 -0500

    Setting default timezone for tests to UTC.

--- phpunit.bootstrap.php
+++ phpunit.bootstrap.php
@@ -1,3 +1,8 @@
 <?php
 
-require_once(dirname(__FILE__).DIRECTORY_SEPARATOR.'autoload.php');
+error_reporting( E_ALL | E_STRICT );
+date_default_timezone_set( 'UTC' );
+
+require_once( 'SymfonyComponents/YAML/sfYaml.php' );
+require_once( 'SymfonyComponents/YAML/sfYamlDumper.php' );
+require_once( dirname(__FILE__).DIRECTORY_SEPARATOR.'autoload.php' );
