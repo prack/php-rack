09106f196363b4948f6b0fa9208c2cd25cb03391
Joshua Morris <php.rack@gmail.com>
2011-03-06 03:43:47 -0500

    Prack_URLMap reworked.

--- prack/urlmap.php
+++ prack/urlmap.php
@@ -9,59 +9,92 @@ class Prack_URLMap
 	private $entries;
 	
 	// TODO: Document!
-	function __construct( $builders )
+	static function with( $hash_map )
 	{
-		$this->remap( $builders );
+		return new Prack_URLMap( $hash_map );
 	}
 	
 	// TODO: Document!
-	public function remap( $builders )
+	function __construct( $hash_map )
 	{
-		$this->entries = array();
+		$this->remap( $hash_map );
+	}
+	
+	// TODO: Document!
+	public function remap( $hash_map )
+	{
+		$callback       = array( $this, 'onRemap' );
+		$proxy_callback = create_function( '$h,$l,$m,$a', '
+		  return Prack::_Array( array (
+		    isset( $h ) ? Prack_Wrapper_Numeric::with( -$h->length() )
+		                : Prack_Wrapper_Numeric::with( -PHP_INT_MAX  ),
+		    Prack_Wrapper_Numeric::with( -$l->length() )
+		  ) );
+		');
+		$result = $hash_map->map( $callback );
 		
-		foreach ( $builders as $builder )
-			array_push( $this->entries, $builder->toArray() );
+		$this->mapping = $result->sortBy( $proxy_callback );
+		return $result;
+	}
+
+	// TODO: Document!
+	public function onRemap( $location, $middleware_app )
+	{
+		$location = Prack::_String( $location );
 		
-		$comparison_function = create_function('$a,$b', 'return strlen( $b[ 0 ].$b[ 1 ] ) - strlen( $a[ 0 ].$a[ 1 ] );');
-		usort( $this->entries, $comparison_function );
+		if ( $location->match( '/\Ahttps?:\/\/(.*?)(\/.*)/', $matches ) )
+		{
+			$host     = Prack::_String( $matches[ 1 ][ 0 ] );
+			$location = Prack::_String( $matches[ 2 ][ 0 ] );
+		}
+		else
+			$host = null;
 		
-		return $this->entries;
+		if ( $location->slice( 0 )->toN() != '/' )
+			throw new Prack_Error_Argument( 'paths need to start with /' );
+		
+		$location   = $location->chomp( Prack::_String( '/' ) );
+		$normalized = preg_replace( '/\//', '\/+', preg_quote( $location->toN() ) );
+		$pattern    = "/\A{$normalized}(.*)/";
+		
+		return Prack::_Array( array(
+		  $host, $location, $pattern, $middleware_app
+		) );
 	}
 	
 	// TODO: Document!
 	// This is where requests get routed.
-	public function call( &$env )
+	public function call( $env )
 	{
-		$env_server_name = $env[ 'SERVER_NAME' ];
-		$env_server_port = $env[ 'SERVER_PORT' ];
-		$env_path        = $env[ 'PATH_INFO'   ];
-		$env_script_name = $env[ 'SCRIPT_NAME' ];
-		$env_http_host   = isset( $env[ 'HTTP_HOST' ] ) ? $env[ 'HTTP_HOST' ] : '';
+		$env_server_name = $env->get( 'SERVER_NAME' );
+		$env_server_port = $env->get( 'SERVER_PORT' );
+		$env_path        = $env->get( 'PATH_INFO'   );
+		$env_script_name = $env->get( 'SCRIPT_NAME' );
+		$env_http_host   = $env->get( 'HTTP_HOST'   );
 		
 		try
 		{
-			foreach ( $this->entries as $entry )
+			foreach ( $this->mapping->toN() as $mapping )
 			{
-				list( $entry_host, $entry_location, $entry_matcher, $entry_middleware_app ) = $entry;
+				list( $mapping_host, $mapping_location, $mapping_matcher, $mapping_middleware_app ) = $mapping->toN();
 				
 				// All the conditions for which we'd consider the request host as a 'match':
-				$entry_host_viable  = ( $entry_host      == $env_http_host );
-				$server_name_viable = ( $env_server_name == $env_http_host );
-				$http_host_viable   = empty( $entry_host ) &&
-				                      ( $env_http_host == $env_server_name ||
-				                        $env_http_host == $env_server_name.':'.$env_server_port );
+				$host_viable = $mapping_host    == $env_http_host ||
+				               $env_server_name == $env_http_host ||
+				               ( is_null( $mapping_host ) &&
+				                      ( $env_http_host->toN() == $env_server_name->toN() ||
+				                        $env_http_host->toN() == $env_server_name->toN().':'.$env_server_port->toN() ) );
 				
 				// Skip the current entry if none of these strategies evaluate to true:
-				if ( !( $entry_host_viable || $server_name_viable || $http_host_viable ) )
-						continue;
+				if ( !$host_viable )
+					continue;
 				
 				// Each entry has a regex pattern to match against. Check if the request URI matches:
-				$matches = array();
-				if ( !( preg_match( $entry_matcher, (string)$env_path, $matches ) && isset( $matches[ 1 ] ) ) )
+				if ( !( preg_match_all( $mapping_matcher, $env_path->toN(), $matches ) > 0 ) )
 					continue;
 				
 				// If the request URI matches, the remainder (i.e. $match[ 1 ]) should start with a '/':
-				if ( !( empty( $matches[ 1 ] ) || substr( $matches[ 1 ], 0, 1 ) == '/' ) )
+				if ( !( empty( $matches[ 1 ][ 0 ] ) || substr( $matches[ 1 ][ 0 ], 0, 1 ) == '/' ) )
 					continue;
 				
 				// If we got here, we found a matching route. Given:
@@ -71,23 +104,35 @@ class Prack_URLMap
 				//   SCRIPT_NAME => '/admin/panel'
 				//   PATH_INFO   => '/foo'
 				// Note that any query string won't make it into PATH_INFO because the web server will put it in QUERY_STRING.
-				$env = array_merge( $env, array( 'SCRIPT_NAME' => $env_script_name.$entry_location, 'PATH_INFO' => $matches[ 1 ] ) );
+				$env->mergeInPlace(
+				  Prack::_Hash( array(
+				    'SCRIPT_NAME' => Prack::_String( $env_script_name->toN().$mapping_location->toN() ),
+				    'PATH_INFO'   => Prack::_String( $matches[ 1 ][ 0 ] )
+				  ) )
+				);
 				
 				// Call the middleware the entry refers to, providing it the newly modified environment.
-				return $entry_middleware_app->call( $env );
+				return $mapping_middleware_app->call( $env );
 			}
-			return array( 404, array( 'Content-Type' => 'text/html', 'X-Cascade' => "pass" ), array( "Not Found: {$env_path}" ) );
+			
+			return array(
+			  404,
+			  Prack::_Hash( array(
+			    'Content-Type' => Prack::_String( 'text/html' ),
+			    'X-Cascade'    => Prack::_String( 'pass' ),
+			  ) ),
+			  Prack::_Array( array( Prack::_String( "Not Found: {$env_path->toN()}" ) ) )
+			);
 		}
 		catch ( Exception $e )
 		{
-			$env = array_merge( $env, array( 'PATH_INFO' => $env_path, 'SCRIPT_NAME' => $env_script_name ) );
-			throw $e;
+			$env->mergeInPlace(
+			  Prack::_Hash( array(
+			    'PATH_INFO'   => $env_path,
+			    'SCRIPT_NAME' => $env_script_name
+			  ) )
+			);
+			throw $e; // Not sure if this is what we want.
 		}
-	}
-	
-	// TODO: Document!
-	public function &getEntries()
-	{
-		return $this->entries;
 	}
 }
--- test/unit/urlmap_test.php
+++ test/unit/urlmap_test.php
@@ -1,171 +1,291 @@
 <?php
 
-require_once join( DIRECTORY_SEPARATOR, array(dirname(__FILE__), '..', 'support', 'samplemiddleware.php') );
 
 // TODO: Document!
 class Prack_URLMapTest extends PHPUnit_Framework_TestCase 
 {
-	private $domain;
-	
 	// TODO: Document!
-	function setUp()
+	static function appWithEval( $eval )
 	{
-		$domain = Prack_Builder::domain();
-		
-		$domain
-		  ->map( '/welcome' )
-		    ->wherein()
-		    ->run( new SampleMiddleware() )
-		  ->map( 'https://localhost/secret/area' )
-		    ->wherein()
-		    ->using( 'SampleMiddleware' )->withArgs()
-		    ->run( new SampleMiddleware() )
-		  ->map( '/' )
-		    ->run( new SampleMiddleware() );
-		
-		$this->domain_as_middleware_app = $domain->toMiddlewareApp();
+		return new Prack_Test_Echo(
+		  200,
+		  Prack::_Hash(),
+		  Prack::_Array( array( Prack::_String() ) ),
+		  $eval
+		);
 	}
 	
 	/**
-	 * instance method remap should transform an indexed array of builders into a lookup table for primitive routing
+	 * It dispatches paths correctly
 	 * @author Joshua Morris
 	 * @test
 	 */
-	public function instance_method_remap_should_transform_an_indexed_array_of_builders_into_a_lookup_table_for_primitive_routing()
+	public function It_dispatches_paths_correctly()
 	{
-		$domain_entries = &$this->domain_as_middleware_app->getEntries();
+		$middleware_app = self::appWithEval('
+		  $this->headers->set( "X-ScriptName",  $env->get( "SCRIPT_NAME" ) );
+		  $this->headers->set( "X-PathInfo",    $env->get( "PATH_INFO"   ) );
+		  $this->headers->set( "X-ContentType", $env->get( "text/plain"  ) );
+		');
 		
-		$this->assertTrue( count( $domain_entries ) == 3 ); // Entry for '/secret/area'
-		
-		// Do some sanity checks on each of the generated entries.
-		foreach ( $domain_entries as $entry )
-		{
-			$this->assertTrue( is_array( $entry ) );
-			$this->assertTrue( count( $entry ) == Prack_URLMap::ENTRY_ELEMENT_COUNT );
-		}
-	} // instance method remap should transform an indexed array of builders into a lookup table for primitive routing
-	
-	/**
-	 * instance method call should route request appropriately and call the associated middleware
-	 * @author Joshua Morris
-	 * @test
-	 */
-	public function instance_method_call_should_route_request_appropriately_and_call_the_associated_middleware()
-	{
-		$domain_entries = &$this->domain_as_middleware_app->getEntries();
-		
-		$env = array(
-			'SCRIPT_NAME' => '',
-			'SERVER_NAME' => 'localhost',
-			'SERVER_PORT' => '80',
-			'HTTP_HOST'   => 'localhost',
-			'PATH_INFO'   => '/secret/area/foo/bar'
+		$url_map = Prack_URLMap::with(
+		  Prack::_Hash( array(
+		    'http://foo.org/bar' => $middleware_app,
+		    '/foo'               => $middleware_app,
+		    '/foo/bar'           => $middleware_app,
+		  ) )
 		);
 		
-		$middleware_mock = $this->getMock( 'SampleMiddleware', array( 'call' ) );
-		$middleware_mock->expects( $this->once() )
-		                ->method( 'call' );
+		$mock_response = Prack_Mock_Request::with( $url_map )->get( Prack::_String( '/' ) );
+		$this->assertTrue( $mock_response->isNotFound() );
 		
-		$secret_area_entry      = &$domain_entries[ 0 ];
-		$request_path           = $secret_area_entry[ 0 ].$secret_area_entry[ 1 ]; // Entry's host concatenated with location
-		$secret_area_entry[ 3 ] = $middleware_mock;                                // Mock the app itself
+		$mock_response = Prack_Mock_Request::with( $url_map )->get( Prack::_String( '/qux' ) );
+		$this->assertTrue( $mock_response->isNotFound() );
 		
-		$this->domain_as_middleware_app->call( $env );
-	} // instance method call should route request appropriately and call the associated middleware
+		$mock_response = Prack_Mock_Request::with( $url_map )->get( Prack::_String( '/foo' ) );
+		$this->assertTrue( $mock_response->isOK() );
+		$this->assertEquals( '/foo', $mock_response->get( 'X-ScriptName' )->toN() );
+		$this->assertEquals( ''    , $mock_response->get( 'X-PathInfo'   )->toN() );
+		
+		$mock_response = Prack_Mock_Request::with( $url_map )->get( Prack::_String( '/foo/' ) );
+		$this->assertTrue( $mock_response->isOK() );
+		$this->assertEquals( '/foo', $mock_response->get( 'X-ScriptName' )->toN() );
+		$this->assertEquals( '/'   , $mock_response->get( 'X-PathInfo'   )->toN() );
+		
+		$mock_response = Prack_Mock_Request::with( $url_map )->get( Prack::_String( '/foo/bar' ) );
+		$this->assertTrue( $mock_response->isOK() );
+		$this->assertEquals( '/foo/bar', $mock_response->get( 'X-ScriptName' )->toN() );
+		$this->assertEquals( ''        , $mock_response->get( 'X-PathInfo'   )->toN() );
+		
+		$mock_response = Prack_Mock_Request::with( $url_map )->get( Prack::_String( '/foo/bar/' ) );
+		$this->assertTrue( $mock_response->isOK() );
+		$this->assertEquals( '/foo/bar', $mock_response->get( 'X-ScriptName' )->toN() );
+		$this->assertEquals( '/'       , $mock_response->get( 'X-PathInfo'   )->toN() );
+		
+		$mock_response = Prack_Mock_Request::with( $url_map )->get( Prack::_String( '/foo///bar//quux' ) );
+		$this->assertTrue( $mock_response->isOK() );
+		$this->assertEquals( '/foo/bar', $mock_response->get( 'X-ScriptName' )->toN() );
+		$this->assertEquals( '//quux'  , $mock_response->get( 'X-PathInfo'   )->toN() );
+		
+		$mock_response = Prack_Mock_Request::with( $url_map )->get(
+		  Prack::_String( '/foo/quux' ),
+		  Prack::_Hash( array( 'SCRIPT_NAME' => Prack::_String( '/bleh' ) ) )
+		);
+		$this->assertTrue( $mock_response->isOK() );
+		$this->assertEquals( '/bleh/foo', $mock_response->get( 'X-ScriptName' )->toN() );
+		$this->assertEquals( '/quux'    , $mock_response->get( 'X-PathInfo'   )->toN() );
+		
+		$mock_response = Prack_Mock_Request::with( $url_map )->get(
+		  Prack::_String( '/bar' ),
+		  Prack::_Hash( array( 'HTTP_HOST' => Prack::_String( 'foo.org' ) ) )
+		);
+		$this->assertTrue( $mock_response->isOK() );
+		$this->assertEquals( '/bar', $mock_response->get( 'X-ScriptName' )->toN() );
+		$this->assertTrue( $mock_response->get( 'X-PathInfo' )->isEmpty() );
+		
+		$mock_response = Prack_Mock_Request::with( $url_map )->get(
+		  Prack::_String( '/bar/' ),
+		  Prack::_Hash( array( 'HTTP_HOST' => Prack::_String( 'foo.org' ) ) )
+		);
+		$this->assertTrue( $mock_response->isOK() );
+		$this->assertEquals( '/bar', $mock_response->get( 'X-ScriptName' )->toN() );
+		$this->assertEquals( '/'   , $mock_response->get( 'X-PathInfo'   )->toN() );
+	} // It dispatches paths correctly
 	
 	/**
-	 * instance method call should not route request to middleware if host is different
+	 * It dispatches hosts correctly
 	 * @author Joshua Morris
 	 * @test
 	 */
-	public function instance_method_call_should_not_route_request_to_middleware_if_host_is_different()
+	public function It_dispatches_hosts_correctly()
 	{
-		$domain_entries = &$this->domain_as_middleware_app->getEntries();
-		
-		$env = array(
-			'SCRIPT_NAME' => '',
-			'SERVER_NAME' => 'myhost.com',
-			'SERVER_PORT' => '80',
-			'HTTP_HOST'   => 'someotherhost.org',
-			'PATH_INFO'   => '/secret/area/foo/bar'
+		$url_map = Prack_URLMap::with(
+		  Prack::_Hash( array( 
+		    'http://foo.org/' => self::appWithEval('
+		      $this->headers->set( "Content-Type", Prack::_String( "text/plain" ) );
+		      $this->headers->set( "X-Position",   Prack::_String( "foo.org"    ) );
+		      $this->headers->set( "X-Host",       $env->contains( "HTTP_HOST") ? $env->get( "HTTP_HOST"   )
+		                                                                        : $env->get( "SERVER_NAME" ) );
+		    '),
+		    'http://subdomain.foo.org/' => self::appWithEval('
+		      $this->headers->set( "Content-Type", Prack::_String( "text/plain"        ) );
+		      $this->headers->set( "X-Position",   Prack::_String( "subdomain.foo.org" ) );
+		      $this->headers->set( "X-Host",       $env->contains( "HTTP_HOST") ? $env->get( "HTTP_HOST"   )
+		                                                                        : $env->get( "SERVER_NAME" ) );
+		    '),
+		    'http://bar.org/' => self::appWithEval('
+		      $this->headers->set( "Content-Type", Prack::_String( "text/plain" ) );
+		      $this->headers->set( "X-Position",   Prack::_String( "bar.org"    ) );
+		      $this->headers->set( "X-Host",       $env->contains( "HTTP_HOST") ? $env->get( "HTTP_HOST"   )
+		                                                                        : $env->get( "SERVER_NAME" ) );
+		    '),
+		    '/' => self::appWithEval('
+		      $this->headers->set( "Content-Type", Prack::_String( "text/plain"  ) );
+		      $this->headers->set( "X-Position",   Prack::_String( "default.org" ) );
+		      $this->headers->set( "X-Host",       $env->contains( "HTTP_HOST" ) ? $env->get( "HTTP_HOST"   )
+		                                                                         : $env->get( "SERVER_NAME" ) );
+		    '),
+		  ) )
 		);
 		
-		$middleware_mock = $this->getMock( 'SampleMiddleware', array( 'call' ) );
-		$middleware_mock->expects( $this->never() )
-		                ->method( 'call' );
+		$mock_response = Prack_Mock_Request::with( $url_map )->get( Prack::_String( '/' ) );
+		$this->assertTrue( $mock_response->isOK() );
+		$this->assertEquals( 'default.org', $mock_response->get( 'X-Position' )->toN() );
 		
-		$secret_area_entry      = &$domain_entries[ 0 ]; // Entry for '/secret/area'
-		$secret_area_entry[ 0 ] = '';                    // Empty the 'host'  field of entry
-		$secret_area_entry[ 3 ] = $middleware_mock;      // Mock the middleware_app
+		$mock_response = Prack_Mock_Request::with( $url_map )->get(
+		  Prack::_String( '/' ),
+		  Prack::_Hash( array( 'HTTP_HOST' => Prack::_String( 'bar.org' ) ) )
+		);
+		$this->assertTrue( $mock_response->isOK() );
+		$this->assertEquals( 'bar.org', $mock_response->get( 'X-Position' )->toN() );
 		
-		$this->domain_as_middleware_app->call( $env );
-	} // instance method call should not route request to middleware if host is different
+		$mock_response = Prack_Mock_Request::with( $url_map )->get(
+		  Prack::_String( '/' ),
+		  Prack::_Hash( array( 'HTTP_HOST' => Prack::_String( 'foo.org' ) ) )
+		);
+		$this->assertTrue( $mock_response->isOK() );
+		$this->assertEquals( 'foo.org', $mock_response->get( 'X-Position' )->toN() );
+		
+		$mock_response = Prack_Mock_Request::with( $url_map )->get(
+		  Prack::_String( '/' ),
+		  Prack::_Hash( array(
+		    'HTTP_HOST'   => Prack::_String( 'subdomain.foo.org' ),
+		    'SERVER_NAME' => Prack::_String( 'foo.org'           )
+		  ) )
+		);
+		$this->assertTrue( $mock_response->isOK() );
+		$this->assertEquals( 'subdomain.foo.org', $mock_response->get( 'X-Position' )->toN() );
+		
+		$mock_response = Prack_Mock_Request::with( $url_map )->get( Prack::_String( 'http://foo.org' ) );
+		$this->assertTrue( $mock_response->isOK() );
+		$this->assertEquals( 'default.org', $mock_response->get( 'X-Position' )->toN() );
+		
+		//FAILS
+		$mock_response = Prack_Mock_Request::with( $url_map )->get(
+		  Prack::_String( '/' ),
+		  Prack::_Hash( array( 'HTTP_HOST' => Prack::_String( 'example.org' ) ) )
+		);
+		$this->assertTrue( $mock_response->isOK() );
+		$this->assertEquals( 'default.org', $mock_response->get( 'X-Position' )->toN() );
+		
+		$mock_response = Prack_Mock_Request::with( $url_map )->get(
+		  Prack::_String( '/' ),
+		  Prack::_Hash( array(
+		    'HTTP_HOST'   => Prack::_String( 'example.org:9292' ),
+		    'SERVER_PORT' => Prack::_String( '9292'             )
+		  ) )
+		);
+		$this->assertTrue( $mock_response->isOK() );
+		$this->assertEquals( 'default.org', $mock_response->get( 'X-Position' )->toN() );
+	} // It dispatches hosts correctly
 	
 	/**
-	 * instance method call should route to the site root if it is mounted as a last resort
+	 * It should be nestable
 	 * @author Joshua Morris
 	 * @test
 	 */
-	public function instance_method_call_should_route_to_the_site_root_if_it_is_mounted_as_a_last_resort()
+	public function It_should_be_nestable()
 	{
-		$domain_entries = &$this->domain_as_middleware_app->getEntries();
+		$url_map = Prack_URLMap::with( Prack::_Hash( array (
+		  '/foo'  => Prack_URLMap::with( Prack::_Hash( array(
+		    '/bar'  => Prack_URLMap::with( Prack::_Hash( array(
+		      '/quux' => self::appWithEval('
+		        $this->headers->set( "Content-Type", Prack::_String( "text/plain"    ) );
+		        $this->headers->set( "X-Position",   Prack::_String( "/foo/bar/quux" ) );
+		        $this->headers->set( "X-PathInfo",   $env->get( "PATH_INFO"   )        );
+		        $this->headers->set( "X-ScriptName", $env->get( "SCRIPT_NAME" )        );
+		      ')
+		    ) ) )
+		  ) ) )
+		) ) );
 		
-		$env = array(
-			'SCRIPT_NAME' => '',
-			'SERVER_NAME' => 'localhost',
-			'SERVER_PORT' => '80',
-			'HTTP_HOST'   => 'localhost',
-			'PATH_INFO'   => '/unmapped/place/foo/bar'
-		);
+		$mock_response = Prack_Mock_Request::with( $url_map )->get( Prack::_String( '/foo/bar' ) );
+		$this->assertTrue( $mock_response->isNotFound() );
 		
-		$middleware_mock = $this->getMock( 'SampleMiddleware', array( 'call' ) );
-		$middleware_mock->expects( $this->once() )
-		                ->method( 'call' );
-		
-		$site_root_entry      = &$domain_entries[ 2 ]; // Entry for '/'
-		$site_root_entry[ 3 ] = $middleware_mock;      // Mock the middleware_app
-		
-		$this->domain_as_middleware_app->call( $env );
-	} // instance method call should route to the site root if it is mounted as a last resort
+		$mock_response = Prack_Mock_Request::with( $url_map )->get( Prack::_String( '/foo/bar/quux' ) );
+		$this->assertTrue( $mock_response->isOK() );
+		$this->assertEquals( '/foo/bar/quux', $mock_response->get( 'X-Position'   )->toN() );
+		$this->assertEquals(              '', $mock_response->get( 'X-PathInfo'   )->toN() );
+		$this->assertEquals( '/foo/bar/quux', $mock_response->get( 'X-ScriptName' )->toN() );
+	} // It should be nestable
 	
 	/**
-	 * instance method call should revert the environment to the original SCRIPT_NAME and PATH_INFO even if the middleware throws an exception
+	 * It should route root apps correctly
 	 * @author Joshua Morris
 	 * @test
 	 */
-	public function instance_method_call_should_revert_the_environment_to_the_original_SCRIPT_NAME_and_PATH_INFO_even_if_the_middleware_throws_an_exception()
+	public function It_should_route_root_apps_correctly()
 	{
-		$domain_entries = &$this->domain_as_middleware_app->getEntries();
-		
-		$original_script_name = '';
-		$original_path_info   = '/secret/area/throwanexception';
-		
-		$env = array(
-			'SCRIPT_NAME' => $original_script_name,
-			'SERVER_NAME' => 'localhost',
-			'SERVER_PORT' => '80',
-			'HTTP_HOST'   => 'localhost',
-			'PATH_INFO'   => $original_path_info
+		$url_map = Prack_URLMap::with(
+		  Prack::_Hash( array( 
+		    '/' => self::appWithEval('
+		      $this->headers->set( "Content-Type", Prack::_String( "text/plain" ) );
+		      $this->headers->set( "X-Position",   Prack::_String( "root"       ) );
+		      $this->headers->set( "X-PathInfo",   $env->get( "PATH_INFO"   )     );
+		      $this->headers->set( "X-ScriptName", $env->get( "SCRIPT_NAME" )     );
+		    '),
+		    '/foo' => self::appWithEval('
+		      $this->headers->set( "Content-Type", Prack::_String( "text/plain" ) );
+		      $this->headers->set( "X-Position",   Prack::_String( "foo"        ) );
+		      $this->headers->set( "X-PathInfo",   $env->get( "PATH_INFO"   )     );
+		      $this->headers->set( "X-ScriptName", $env->get( "SCRIPT_NAME" )     );
+		    '),
+		  ) )
 		);
 		
-		$middleware_mock = $this->getMock( 'SampleMiddleware', array( 'call' ) );
-		$middleware_mock->expects( $this->once() )
-		                ->method( 'call' )
-		                ->will( $this->throwException( new Exception() ) );
+		$mock_response = Prack_Mock_Request::with( $url_map )->get( Prack::_String( '/foo/bar' ) );
+		$this->assertTrue( $mock_response->isOK() );
+		$this->assertEquals(  'foo', $mock_response->get( 'X-Position'   )->toN() );
+		$this->assertEquals( '/bar', $mock_response->get( 'X-PathInfo'   )->toN() );
+		$this->assertEquals( '/foo', $mock_response->get( 'X-ScriptName' )->toN() );
+
+		$mock_response = Prack_Mock_Request::with( $url_map )->get( Prack::_String( '/foo' ) );
+		$this->assertTrue( $mock_response->isOK() );
+		$this->assertEquals(  'foo', $mock_response->get( 'X-Position'   )->toN() );
+		$this->assertEquals(     '', $mock_response->get( 'X-PathInfo'   )->toN() );
+		$this->assertEquals( '/foo', $mock_response->get( 'X-ScriptName' )->toN() );
 		
-		$site_root_entry      = &$domain_entries[ 0 ]; // Entry for '/secret/area'
-		$site_root_entry[ 3 ] = $middleware_mock;      // Mock the middleware_app
+		$mock_response = Prack_Mock_Request::with( $url_map )->get( Prack::_String( '/bar' ) );
+		$this->assertTrue( $mock_response->isOK() );
+		$this->assertEquals( 'root', $mock_response->get( 'X-Position'   )->toN() );
+		$this->assertEquals( '/bar', $mock_response->get( 'X-PathInfo'   )->toN() );
+		$this->assertEquals(     '', $mock_response->get( 'X-ScriptName' )->toN() );
 		
-		try
-		{
-			$this->domain_as_middleware_app->call( $env );
-		}
-		catch ( Exception $e )
-		{
-			// Don't need to do anything here.
-		}
+		$mock_response = Prack_Mock_Request::with( $url_map )->get( Prack::_String() );
+		$this->assertTrue( $mock_response->isOK() );
+		$this->assertEquals( 'root', $mock_response->get( 'X-Position'   )->toN() );
+		$this->assertEquals(    '/', $mock_response->get( 'X-PathInfo'   )->toN() );
+		$this->assertEquals(     '', $mock_response->get( 'X-ScriptName' )->toN() );
+	} // It should route root apps correctly
+	
+	/**
+	 * It should not squeeze slashes
+	 * @author Joshua Morris
+	 * @test
+	 */
+	public function It_should_not_squeeze_slashes()
+	{
+		$url_map = Prack_URLMap::with(
+		  Prack::_Hash( array( 
+		    '/' => self::appWithEval('
+		      $this->headers->set( "Content-Type", Prack::_String( "text/plain" ) );
+		      $this->headers->set( "X-Position",   Prack::_String( "root"       ) );
+		      $this->headers->set( "X-PathInfo",   $env->get( "PATH_INFO"   )     );
+		      $this->headers->set( "X-ScriptName", $env->get( "SCRIPT_NAME" )     );
+		    '),
+		    '/foo' => self::appWithEval('
+		      $this->headers->set( "Content-Type", Prack::_String( "text/plain" ) );
+		      $this->headers->set( "X-Position",   Prack::_String( "foo"        ) );
+		      $this->headers->set( "X-PathInfo",   $env->get( "PATH_INFO"   )     );
+		      $this->headers->set( "X-ScriptName", $env->get( "SCRIPT_NAME" )     );
+		    '),
+		  ) )
+		);
 		
-		$this->assertEquals( $original_script_name, $env[ 'SCRIPT_NAME' ] );
-		$this->assertEquals( $original_path_info  , $env[ 'PATH_INFO' ]   );
-	} // instance method call should revert the environment to the original SCRIPT_NAME and PATH_INFO even if the middleware throws an exception
+		$mock_response = Prack_Mock_Request::with( $url_map )->get( Prack::_String( '/http://example.org/bar' ) );
+		$this->assertTrue( $mock_response->isOK() );
+		$this->assertEquals(                    'root', $mock_response->get( 'X-Position'   )->toN() );
+		$this->assertEquals( '/http://example.org/bar', $mock_response->get( 'X-PathInfo'   )->toN() );
+		$this->assertEquals(                        '', $mock_response->get( 'X-ScriptName' )->toN() );
+	} // It should not squeeze slashes
 }
