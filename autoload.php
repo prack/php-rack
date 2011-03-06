3089e26065d89487acd98982cc4d8208ee3dbef4
Joshua Morris <php.rack@gmail.com>
2011-03-06 03:24:31 -0500

    Bootstrapping and autoloading reworked.

--- autoload.php
+++ autoload.php
@@ -5,12 +5,13 @@ function autoloadPrackClasses( $class_or_interface ) {
 	$path_components = preg_split( '/_/', $class_or_interface );
 	$path_components = array_map( 'strtolower', $path_components );
 
-	$path = implode( DIRECTORY_SEPARATOR, $path_components ).'.php';
-	if ( file_exists( $path ) && !is_dir( $path ))
-		include $path;
+	$rel = implode( DIRECTORY_SEPARATOR, $path_components ).'.php';
+	$abs = join( DIRECTORY_SEPARATOR, array( dirname( __FILE__ ), $rel ) );
 	
-	if ( !class_exists( $class_or_interface ) && !interface_exists( $class_or_interface ) )
-		throw new RuntimeException( "Unable to load class {$class_or_interface} at {$path}." );
+	if ( file_exists( $abs ) && !is_dir( $abs ) )
+		include $abs;
+	else
+		throw new RuntimeException( "Unable to load class {$class_or_interface} at {$abs}." );
 }
 
 spl_autoload_register( 'autoloadPrackClasses' );
--- phpunit.bootstrap.php
+++ phpunit.bootstrap.php
@@ -3,4 +3,5 @@
 error_reporting( E_ALL | E_STRICT );
 date_default_timezone_set( 'UTC' );
 
-require_once( dirname(__FILE__).DIRECTORY_SEPARATOR.'autoload.php' );
+require_once( dirname( __FILE__ ).DIRECTORY_SEPARATOR.'autoload.php' );
+require_once join( DIRECTORY_SEPARATOR, array( dirname( __FILE__ ), 'test', 'testhelper.php' ) );
