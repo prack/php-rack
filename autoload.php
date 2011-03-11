71e525bf8ed83cc4abe62404f0ae2da5d6506d17
Joshua Morris <php.rack@gmail.com>
2011-03-10 20:03:34 -0500

    Autoload and PHPUnit configuration update.

--- autoload.php
+++ autoload.php
@@ -1,17 +1,15 @@
 <?php
 
 // TODO: Document!
-function autoloadPrackClasses( $class_or_interface ) {
+function __prack_autoload( $class_or_interface ) {
 	$path_components = preg_split( '/_/', $class_or_interface );
 	$path_components = array_map( 'strtolower', $path_components );
-
-	$rel = implode( DIRECTORY_SEPARATOR, $path_components ).'.php';
-	$abs = join( DIRECTORY_SEPARATOR, array( dirname( __FILE__ ), $rel ) );
 	
+	$rel = implode( DIRECTORY_SEPARATOR, $path_components ).'.php';
+	$abs = join( DIRECTORY_SEPARATOR, array( dirname( __FILE__ ), 'lib', $rel ) );
+
 	if ( file_exists( $abs ) && !is_dir( $abs ) )
 		include $abs;
-	else
-		throw new RuntimeException( "Unable to load class {$class_or_interface} at {$abs}." );
 }
 
-spl_autoload_register( 'autoloadPrackClasses' );
+spl_autoload_register( '__prack_autoload', true );
--- phpunit.bootstrap.php
+++ phpunit.bootstrap.php
@@ -3,5 +3,26 @@
 error_reporting( E_ALL | E_STRICT );
 date_default_timezone_set( 'UTC' );
 
-require_once( dirname( __FILE__ ).DIRECTORY_SEPARATOR.'autoload.php' );
-require_once join( DIRECTORY_SEPARATOR, array( dirname( __FILE__ ), 'test', 'testhelper.php' ) );
+$pwd = dirname( __FILE__ );
+
+// Prack depends on Prb to run. We have to load its autoloader.
+//
+// This bootstrapper assumes the following filestructure to work:
+//   (enclosing folder, possibly a /lib directory )
+//   \_ php-rack (you are here, in phpunit.bootstrap.php.)
+//   \_ php-rb  
+//
+// Consequently, it looks in '../php-rb/' for 'autoload.php'.
+// You can override this by setting the environment variable
+// PRACK_PRB_AUTOLOAD_PATH like so, when you run your tests:
+//   $ PRACK_PRB_AUTOLOAD_PATH="/path/to/php-rb/autoload.php" phpunit
+$prb_autoload_file_path =
+  getenv( 'PRACK_PRB_AUTOLOAD_PATH' ) ? getenv( 'PRACK_PRB_AUTOLOAD_PATH' )
+                                      : join( DIRECTORY_SEPARATOR, array( $pwd, '..', 'php-rb', 'autoload.php' ) );
+
+if ( !file_exists( $prb_autoload_file_path ) )
+	die( 'Cannot load php-rb autoload file. See phpunit.bootstrap.php source for instructions.' );
+
+require_once $prb_autoload_file_path;
+require_once join( DIRECTORY_SEPARATOR, array( $pwd, 'autoload.php'       ) );
+require_once join( DIRECTORY_SEPARATOR, array( $pwd, 'test', 'helper.php' ) );
