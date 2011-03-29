908f732f87644207ab04ec3ee4da607709a8b2ba
Joshua Morris <php.rack@gmail.com>
2011-03-29 18:37:58 -0400

    Prack_TestHelper: redesigned with PHP primitives.

--- test/helper.php
+++ test/helper.php
@@ -5,29 +5,22 @@ class Prack_Test_EnvSerializer
   implements Prack_I_MiddlewareApp
 {
 	// TODO: Document!
-	public function call( $env )
+	public function call( &$env )
 	{
-		$request = new Prack_Request( $env );
+		$env[ 'mock.postdata' ] = $env[ 'rack.input' ]->read();
 		
-		$env->set( 'mock.postdata', $env->get( 'rack.input' )->read() );
-		$get_params = $request->GET();
-		
-		if ( $get_params->contains( 'error' ) )
+		$get_params = Prack_Request::with( $env )->GET();
+		if ( @$get_params[ 'error' ] )
 		{
-			$env->get( 'rack.errors' )->puts( $get_params->get( 'error' ) );
-			$env->get( 'rack.errors' )->flush();
+			$env[ 'rack.errors' ]->puts( $get_params[ 'error' ] );
+			$env[ 'rack.errors' ]->flush();
 		}
 		
-		$status = $get_params->contains( 'status' ) ? $get_params->get( 'status' )->toN()
-		                                            : Prb::Num( 200 );
+		$body    = serialize( $env );
+		$status  = @$get_params[ 'status' ] ? $get_params[ 'status' ] : 200;
+		$headers = array( 'Content-Type' => 'text/yaml' );
 		
-		$response = new Prack_Response(
-		  Prb::Str( serialize( $env ) ),
-		  $status,
-		  Prb::Hsh( array( 'Content-Type' => Prb::Str( 'text/yaml' ) ) )
-		);
-		
-		return $response->toA();
+		return Prack_Response::with( $body, $status, $headers )->raw();
 	}
 }
 
@@ -41,35 +34,28 @@ class Prack_Test_Echo
 	private $eval;
 	
 	// TODO: Document!
-	function __construct( $status = null, $headers = null, $body = null, $eval = null )
+	function __construct( $status = 200, $headers = array(), $body = array(), $eval = null )
 	{
-		$status  = is_null( $status ) ? Prb::Num( 200 ) : $status;
-		$headers = is_null( $headers )
-		  ? Prb::Hsh( array(
-		      'Content-Type' => Prb::Str( 'test/plain' )
-		    ) )
-		  : $headers;
-		$body = is_null( $body )
-		  ? Prb::Ary( array( Prb::Str() ) )
-		  : $body;
+		$status  = is_null( $status  ) ? 200 : (int)$status;
+		$headers = is_null( $headers ) ? array( 'Content-Type' => 'test/plain' ) : (array)$headers;
+		$eval    = is_null( $eval    ) ? '' : $eval;
 		
-		$eval = is_null( $eval ) ? '' : $eval;
 		if ( !is_string( $eval ) )
-			throw new Prb_Exception_Type( 'FAILSAFE: __construct $eval must be php-native string' );
+			throw new Prb_Exception_Type( 'FAILSAFE: __construct $eval must be string' );
 		
 		$this->status  = $status;
-		$this->headers = Prack_Utils_HeaderHash::using( $headers );
+		$this->headers = $headers;
 		$this->body    = $body;
 		$this->eval    = $eval;
 	}
 	
 	// TODO: Document!
-	public function call( $env )
+	public function call( &$env )
 	{
 		if ( $this->eval )
 			eval( $this->eval );
 		
-		return Prb::Ary( array( $this->status, $this->headers, $this->body ) );
+		return array( $this->status, $this->headers, $this->body );
 	}
 	
 	public function setEval( $eval )
@@ -88,6 +74,6 @@ class Prack_TestHelper
 		$out  = '';
 		for( $c = 0; $c < $length; $c++ )
 			$out .= (string)$aZ09[ mt_rand( 0, count( $aZ09 ) - 1 ) ];
-		return Prb::Str( $out );
+		return $out;
 	}
 }
